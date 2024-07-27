e5d738a65a05ddb12b71a77f19e8fc9b4386ac9b
Ed <4785890+edwardfward@users.noreply.github.com>
2024-07-26 21:26:56 -0500

    implementing last velocity, next unit tests

--- velocitymps.go
+++ velocitymps.go
@@ -1,5 +1,89 @@
 package wx
 
+import "fmt"
+
+// VelocityMps represents a velocity in meters per second.
 type VelocityMps struct {
 	measurement float64
+}
+
+// Fps returns the speed in feet per second.
+func (v *VelocityMps) Fps() float64 {
+	return v.measurement * feetPerMeter
+}
+
+// Kts returns the speed in knots.
+func (v *VelocityMps) Kts() float64 {
+	return v.Fps() / feetPerMeter * feetPerNauticalMile / secondsInHour
+}
+
+// Kph returns the speed in kilometers per hour.
+func (v *VelocityMps) Kph() float64 {
+	return v.measurement * secondsInHour / 1000
+}
+
+// Mph returns the speed in miles per hour.
+func (v *VelocityMps) Mph() float64 {
+	return v.Fps() / feetPerStatuteMile * secondsInHour
+}
+
+// Mps returns the speed in meters per second.
+func (v *VelocityMps) Mps() float64 {
+	return v.measurement
+}
+
+// Set sets the velocity to the specified value.
+func (v *VelocityMps) Set(measurement float64) error {
+	if measurement < 0 {
+		v.measurement = 0
+
+		return fmt.Errorf(ErrVelocityMustBeZeroOrGreater)
+	}
+
+	v.measurement = measurement
+
+	return nil
+}
+
+// ToFps converts the speed to feet per second.
+func (v *VelocityMps) ToFps() VelocityFps {
+	return VelocityFps{
+		measurement: v.Fps(),
+	}
+}
+
+// ToKts converts the speed to knots.
+func (v *VelocityMps) ToKts() VelocityKts {
+	return VelocityKts{
+		measurement: v.Kts(),
+	}
+}
+
+// ToKph converts the speed to kilometers per hour.
+func (v *VelocityMps) ToKph() VelocityKph {
+	return VelocityKph{
+		measurement: v.Kph(),
+	}
+}
+
+// ToMph converts the speed to miles per hour.
+func (v *VelocityMps) ToMph() VelocityMph {
+	return VelocityMph{
+		measurement: v.Mph(),
+	}
+}
+
+// ToMps converts the speed to meters per second.
+func (v *VelocityMps) ToMps() VelocityMps {
+	return *v
+}
+
+// Units return the units of the speed.
+func (v *VelocityMps) Units() VelocityUnit {
+	return MetersPerSecond
+}
+
+// Valid returns true if the velocity is zero or greater.
+func (v *VelocityMps) Valid() bool {
+	return v.measurement >= 0
 }
--- velocitymps_test.go
+++ velocitymps_test.go
@@ -1,1 +1,8 @@
 package wx
+
+import "testing"
+
+func TestVelocityMps_ImplementVelocity(t *testing.T) {
+	var v Velocity = &VelocityMps{}
+	_ = v
+}
