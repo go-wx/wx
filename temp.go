03da9ea182cc51505bb2d3fc6dbe5f8d23b8be9c
Ed <4785890+edwardfward@users.noreply.github.com>
2024-09-19 21:21:46 -0500

    refactoring temp

--- temp.go
+++ temp.go
@@ -1,33 +1,211 @@
 package wx
 
-// TempUnit is a type for temperature units.
+import "fmt"
+
+// TempUnit represents a unit of temperature.
+type tempType int
+
+// String returns the string representation of the temperature unit.
+func (t tempType) String() string {
+	switch t {
+	case celsius:
+		return "°C"
+	case fahrenheit:
+		return "°F"
+	case kelvin:
+		return "K"
+	case rankine:
+		return "R"
+	}
+	return ""
+}
+
+// Temperature units.
+const (
+	celsius tempType = iota
+	fahrenheit
+	kelvin
+	rankine
+)
+
+// TempUnit represents a unit of temperature.
 type TempUnit struct {
-	string
+	tempType
+}
+
+func (t TempUnit) String() string {
+	return t.tempType.String()
 }
 
 // Temperature units.
 var (
-	Celsius    = TempUnit{"C"}
-	Fahrenheit = TempUnit{"F"}
-	Kelvin     = TempUnit{"K"}
-	Rankine    = TempUnit{"R"}
+	Celsius    = TempUnit{celsius}
+	Fahrenheit = TempUnit{fahrenheit}
+	Kelvin     = TempUnit{kelvin}
+	Rankine    = TempUnit{rankine}
 )
 
-// Temp is an interface for temperature measurements.
-type Temp interface {
-	C() float64
-	F() float64
-	K() float64
-	Set(measurement float64) error
-	ToC() TempC
-	ToF() TempF
-	ToK() TempK
-	ToR() TempR
-	Units() TempUnit
-	Valid() bool
+const (
+	// Absolute zero in Celsius.
+	absoluteZeroC = -273.15
+	// Absolute zero in Fahrenheit.
+	absoluteZeroF = -459.67
+	// Absolute zero in Kelvin.
+	absoluteZeroK = 0
+	// Absolute zero in Rankine.
+	absoluteZeroR = 0
+	// Freezing point of water in Celsius.
+	freezingC = 0
+	// Freezing point of water in Fahrenheit.
+	freezingF = 32
+	// Conversion factor from Celsius to Fahrenheit.
+	cToF = 9.0 / 5.0
+	// Conversion factor from Fahrenheit to Celsius.
+	fToC = 5.0 / 9.0
+)
+
+// Temp is a temperature measurement.
+type Temp struct {
+	measurement float64  // All temperatures are stored in the same unit.
+	unit        TempUnit // The unit of the temperature.
+	valid       bool     // True if the temperature is valid.
 }
 
-// String returns the string representation of the temperature unit.
-func (u TempUnit) String() string {
-	return u.string
+func (t Temp) String() string {
+	switch t.unit.tempType {
+	case celsius:
+		if t.valid {
+			return fmt.Sprintf("%.1f°C", t.measurement)
+		}
+		return "invalid °C"
+	case fahrenheit:
+		if t.valid {
+			return fmt.Sprintf("%.1f°F", t.measurement)
+		}
+		return "invalid °F"
+	case kelvin:
+		if t.valid {
+			return fmt.Sprintf("%.1f K", t.measurement)
+		}
+		return "invalid K"
+	case rankine:
+		if t.valid {
+			return fmt.Sprintf("%.1f R", t.measurement)
+		}
+		return "invalid R"
+	}
+
+	return "invalid temp unit"
+}
+
+// Valid returns true if the temperature is valid.
+func (t Temp) Valid() bool {
+	return t.valid
+}
+
+// NewTemp creates a new temperature measurement.
+func NewTemp(measurement float64, unit TempUnit) Temp {
+	return Temp{measurement,
+		unit,
+		validMeasurement(measurement, unit)}
+}
+
+// C returns the temperature in Celsius.
+func (t Temp) C() float64 {
+	switch t.unit.tempType {
+	case celsius:
+		return t.measurement
+	case fahrenheit:
+		return (t.measurement - freezingF) * fToC
+	case kelvin:
+		return t.measurement + absoluteZeroC
+	case rankine:
+		return (t.measurement / cToF) + absoluteZeroC
+	}
+
+	return 0
+}
+
+// ToC converts the temperature to Celsius.
+func (t Temp) ToC() Temp {
+	return NewTemp(t.C(), Celsius)
+}
+
+// F returns the temperature in Fahrenheit.
+func (t Temp) F() float64 {
+	switch t.unit.tempType {
+	case celsius:
+		return t.measurement*cToF + freezingF
+	case fahrenheit:
+		return t.measurement
+	case kelvin:
+		return t.measurement*cToF + absoluteZeroF
+	case rankine:
+		return t.measurement + absoluteZeroF
+	}
+
+	return 0
+}
+
+// ToF converts the temperature to Fahrenheit.
+func (t Temp) ToF() Temp {
+	return NewTemp(t.F(), Fahrenheit)
+}
+
+// K returns the temperature in Kelvin.
+func (t Temp) K() float64 {
+	switch t.unit.tempType {
+	case celsius:
+		return t.measurement - absoluteZeroC
+	case fahrenheit:
+		return (t.measurement - absoluteZeroF) * fToC
+	case kelvin:
+		return t.measurement
+	case rankine:
+		return t.measurement * fToC
+	}
+
+	return 0
+}
+
+// ToK converts the temperature to Kelvin.
+func (t Temp) ToK() Temp {
+	return NewTemp(t.K(), Kelvin)
+}
+
+// R returns the temperature in Rankine.
+func (t Temp) R() float64 {
+	switch t.unit.tempType {
+	case celsius:
+		return (t.measurement - absoluteZeroC) * cToF
+	case fahrenheit:
+		return t.measurement - absoluteZeroF
+	case kelvin:
+		return t.measurement * cToF
+	case rankine:
+		return t.measurement
+	}
+
+	return 0
+}
+
+// ToR converts the temperature to Rankine.
+func (t Temp) ToR() Temp {
+	return NewTemp(t.R(), Rankine)
+}
+
+// validMeasurement returns true if the measurement is valid
+// for the given temperature unit.
+func validMeasurement(measurement float64, unit TempUnit) bool {
+	switch unit {
+	case Celsius:
+		return measurement >= absoluteZeroC
+	case Fahrenheit:
+		return measurement >= absoluteZeroF
+	case Kelvin:
+		return measurement >= absoluteZeroK
+	case Rankine:
+		return measurement >= absoluteZeroR
+	}
+	return false
 }
--- temp_test.go
+++ temp_test.go
@@ -2,21 +2,299 @@ package wx
 
 import "testing"
 
-func TestTempUnit_String(t *testing.T) {
-	tests := []struct {
+func TestTempUnitString(t *testing.T) {
+	tt := []struct {
 		name string
-		u    TempUnit
+		unit TempUnit
 		want string
 	}{
-		{"Celsius", Celsius, "C"},
-		{"Fahrenheit", Fahrenheit, "F"},
-		{"Kelvin", Kelvin, "K"},
-		{"Rankine", Rankine, "R"},
+		{"celsius", Celsius, "°C"},
+		{"fahrenheit", Fahrenheit, "°F"},
+		{"kelvin", Kelvin, "K"},
+		{"rankine", Rankine, "R"},
+		{"invalid", TempUnit{-1}, ""},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.u.String(); got != tt.want {
-				t.Errorf("TempUnit.String() = %v, want %v", got, tt.want)
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.unit.String() != tc.want {
+				t.Errorf("got %v, want %v", tc.unit.String(), tc.want)
+			}
+		})
+	}
+}
+
+func TestNewTemp(t *testing.T) {
+	tt := []struct {
+		name        string
+		measurement float64
+		unit        TempUnit
+		valid       bool
+	}{
+		{"valid celsius", 0, Celsius, true},
+		{"valid fahrenheit", 0, Fahrenheit, true},
+		{"valid kelvin", 0, Kelvin, true},
+		{"valid rankine", 0, Rankine, true},
+		{"invalid celsius", -274, Celsius, false},
+		{"invalid fahrenheit", -460, Fahrenheit, false},
+		{"invalid kelvin", -1, Kelvin, false},
+		{"invalid rankine", -1, Rankine, false},
+		{"invalid unit", 0, TempUnit{-1}, false},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			temp := NewTemp(tc.measurement, tc.unit)
+			if temp.valid != tc.valid {
+				t.Errorf("got %v, want %v", temp.valid, tc.valid)
+			}
+		})
+	}
+}
+
+func TestTempString(t *testing.T) {
+	tt := []struct {
+		name        string
+		measurement float64
+		unit        TempUnit
+		want        string
+	}{
+		{"celsius", 0, Celsius, "0.0°C"},
+		{"fahrenheit", 0, Fahrenheit, "0.0°F"},
+		{"kelvin", 0, Kelvin, "0.0 K"},
+		{"rankine", 0, Rankine, "0.0 R"},
+		{"invalid", 0, TempUnit{-1}, "invalid temp unit"},
+		{"invalid celsius", -274, Celsius, "invalid °C"},
+		{"invalid fahrenheit", -460, Fahrenheit, "invalid °F"},
+		{"invalid kelvin", -1, Kelvin, "invalid K"},
+		{"invalid rankine", -1, Rankine, "invalid R"},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			temp := NewTemp(tc.measurement, tc.unit)
+			if temp.String() != tc.want {
+				t.Errorf("got %v, want %v", temp.String(), tc.want)
+			}
+		})
+	}
+}
+
+func TestTempValid(t *testing.T) {
+	tt := []struct {
+		name        string
+		measurement float64
+		unit        TempUnit
+		valid       bool
+	}{
+		{"valid celsius", freezingC, Celsius, true},
+		{"valid fahrenheit", freezingF, Fahrenheit, true},
+		{"valid kelvin", absoluteZeroK, Kelvin, true},
+		{"valid rankine", absoluteZeroR, Rankine, true},
+		{"invalid celsius", -274, Celsius, false},
+		{"invalid fahrenheit", -460, Fahrenheit, false},
+		{"invalid kelvin", -1, Kelvin, false},
+		{"invalid rankine", -1, Rankine, false},
+		{"invalid unit", 0, TempUnit{-1}, false},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			temp := NewTemp(tc.measurement, tc.unit)
+			if temp.Valid() != tc.valid {
+				t.Errorf("got %v, want %v", temp.Valid(), tc.valid)
+			}
+		})
+	}
+}
+
+func TestTempC(t *testing.T) {
+	tt := []struct {
+		name        string
+		measurement float64
+		unit        TempUnit
+		want        float64
+	}{
+		{"celsius", freezingC, Celsius, freezingC},
+		{"fahrenheit", freezingF, Fahrenheit, freezingC},
+		{"kelvin", 0, Kelvin, absoluteZeroC},
+		{"rankine", 0, Rankine, absoluteZeroC},
+		{"invalid", 0, TempUnit{-1}, 0},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			temp := NewTemp(tc.measurement, tc.unit)
+			if temp.C() != tc.want {
+				t.Errorf("got %v, want %v", temp.C(), tc.want)
+			}
+		})
+	}
+}
+
+func TestTempToC(t *testing.T) {
+	tt := []struct {
+		name        string
+		measurement float64
+		unit        TempUnit
+		want        float64
+	}{
+		{"celsius", freezingC, Celsius, freezingC},
+		{"fahrenheit", freezingF, Fahrenheit, freezingC},
+		{"kelvin", absoluteZeroK, Kelvin, absoluteZeroC},
+		{"rankine", absoluteZeroR, Rankine, absoluteZeroC},
+		{"invalid", 0, TempUnit{-1}, 0},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			temp := NewTemp(tc.measurement, tc.unit)
+			if temp.ToC().C() != tc.want {
+				t.Errorf("got %v, want %v", temp.ToC().C(), tc.want)
+			}
+		})
+	}
+}
+
+func TestTempF(t *testing.T) {
+	tt := []struct {
+		name        string
+		measurement float64
+		unit        TempUnit
+		want        float64
+	}{
+		{"celsius", freezingC, Celsius, freezingF},
+		{"fahrenheit", freezingF, Fahrenheit, freezingF},
+		{"kelvin", absoluteZeroK, Kelvin, absoluteZeroF},
+		{"rankine", absoluteZeroR, Rankine, absoluteZeroF},
+		{"invalid", 0, TempUnit{-1}, 0},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			temp := NewTemp(tc.measurement, tc.unit)
+			if temp.F() != tc.want {
+				t.Errorf("got %v, want %v", temp.F(), tc.want)
+			}
+		})
+	}
+}
+
+func TestTempToF(t *testing.T) {
+	tt := []struct {
+		name        string
+		measurement float64
+		unit        TempUnit
+		want        float64
+	}{
+		{"celsius", freezingC, Celsius, freezingF},
+		{"fahrenheit", freezingF, Fahrenheit, freezingF},
+		{"kelvin", absoluteZeroK, Kelvin, absoluteZeroF},
+		{"rankine", absoluteZeroR, Rankine, absoluteZeroF},
+		{"invalid", 0, TempUnit{-1}, 0},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			temp := NewTemp(tc.measurement, tc.unit)
+			if temp.ToF().F() != tc.want {
+				t.Errorf("got %v, want %v", temp.ToF().F(), tc.want)
+			}
+		})
+	}
+}
+
+func TestTempK(t *testing.T) {
+	tt := []struct {
+		name        string
+		measurement float64
+		unit        TempUnit
+		want        float64
+	}{
+		{"celsius", absoluteZeroC, Celsius, absoluteZeroK},
+		{"fahrenheit", absoluteZeroF, Fahrenheit, absoluteZeroK},
+		{"kelvin", absoluteZeroK, Kelvin, absoluteZeroK},
+		{"rankine", absoluteZeroR, Rankine, absoluteZeroK},
+		{"invalid", 0, TempUnit{-1}, 0},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			temp := NewTemp(tc.measurement, tc.unit)
+			if temp.K() != tc.want {
+				t.Errorf("got %v, want %v", temp.K(), tc.want)
+			}
+		})
+	}
+}
+
+func TestTempToK(t *testing.T) {
+	tt := []struct {
+		name        string
+		measurement float64
+		unit        TempUnit
+		want        float64
+	}{
+		{"celsius", absoluteZeroC, Celsius, absoluteZeroK},
+		{"fahrenheit", absoluteZeroF, Fahrenheit, absoluteZeroK},
+		{"kelvin", absoluteZeroK, Kelvin, absoluteZeroK},
+		{"rankine", absoluteZeroR, Rankine, absoluteZeroK},
+		{"invalid", 0, TempUnit{-1}, 0},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			temp := NewTemp(tc.measurement, tc.unit)
+			if temp.ToK().K() != tc.want {
+				t.Errorf("got %v, want %v", temp.ToK().K(), tc.want)
+			}
+		})
+	}
+}
+
+func TestTempR(t *testing.T) {
+	tt := []struct {
+		name        string
+		measurement float64
+		unit        TempUnit
+		want        float64
+	}{
+		{"celsius", absoluteZeroC, Celsius, absoluteZeroR},
+		{"fahrenheit", absoluteZeroF, Fahrenheit, absoluteZeroR},
+		{"kelvin", absoluteZeroK, Kelvin, absoluteZeroR},
+		{"rankine", absoluteZeroR, Rankine, absoluteZeroR},
+		{"invalid", 0, TempUnit{-1}, 0},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			temp := NewTemp(tc.measurement, tc.unit)
+			if temp.R() != tc.want {
+				t.Errorf("got %v, want %v", temp.R(), tc.want)
+			}
+		})
+	}
+}
+
+func TestTempToR(t *testing.T) {
+	tt := []struct {
+		name        string
+		measurement float64
+		unit        TempUnit
+		want        float64
+	}{
+		{"celsius", absoluteZeroC, Celsius, absoluteZeroR},
+		{"fahrenheit", absoluteZeroF, Fahrenheit, absoluteZeroR},
+		{"kelvin", absoluteZeroK, Kelvin, absoluteZeroR},
+		{"rankine", absoluteZeroR, Rankine, absoluteZeroR},
+		{"invalid", 0, TempUnit{-1}, 0},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			temp := NewTemp(tc.measurement, tc.unit)
+			if temp.ToR().R() != tc.want {
+				t.Errorf("got %v, want %v", temp.ToR().R(), tc.want)
 			}
 		})
 	}
